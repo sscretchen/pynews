141ede1b6e02966be286e3aa9104dc4693dd1678
sscretchen <sjscretchen@gmail.com>
2020-12-03 00:47:14 -0500

    Integrated first API

--- news/urls.py
+++ news/urls.py
@@ -4,4 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', index, name="index"),
+    path('about/', about, name="about"),
+    # Need unique ID for each post later ex: <int:pk>
+    path('post_detail/', post_detail, name="post_detail"),
 ]
--- news/views.py
+++ news/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
-
+import requests
+import json
 
 def index(request):
     """Main view of site"""
-    return render(request, 'index.html')
+    tech_api_request = requests.get("http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=e8c53fe0ff3d44da82a8efc233d8247d")
+    api = json.loads(tech_api_request.content)
+
+    context = {
+        'api': api,
+    }
+    return render(request, 'news/index.html', context)
+
+
+def about(request):
+    """Overview page of site"""
+    return render(request, 'news/about.html')
+
+
+def post_detail(request):
+    """Single page view of each post"""
+    return render(request, 'news/post_detail.html')
